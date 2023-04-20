b1f77f58760f594e543cd2abcc1781193fb9e872
agerada <sandrogerada@gmail.com>
2023-04-20 15:09:47 +0100

    Not working - trying new printing mode

--- main.py
+++ main.py
@@ -76,14 +76,61 @@ class PaperNode(Paper, NodeMixin):
     def __init__(self, DOI, title, author, year, references = None, parent = None, children = None):
         super().__init__(DOI, title, author, year, references)
         self.name = self.make_name()
-        self._parent = parent
-        self._children = children
+        self.parent = parent
+        if children:
+            self.children = children
     
     # let's not use add_children for now
-    def add_children(self):
-        if self._references:
-            for reference in self._references:
-                self._children.append = reference        
+    def add_children(self, children):
+        if not children: 
+            return
+        self.children = children
+        """if not self.children: 
+            if len(children) == 1: 
+                self.children = []
+                self.children.append(children)
+            else: 
+                self.children = children
+        else: 
+            if len(children) == 1: 
+                self.children.append(children)
+            else: 
+                for i in children: 
+                    self.children.append(i)"""
+
+    def add_child_node(self, child_node): 
+        if not child_node: 
+            return
+        if type(child_node) != PaperNode: 
+            raise ValueError("Function .add_child_node() can only take PaperNode for parameter child_node") 
+        if child_node in self.get_children(): 
+            return
+        previous_children = self.get_children()
+        new_children = list(previous_children)
+        new_children.append(child_node)
+        self.children = new_children
+
+        """
+        if not self.children: 
+            self.children = []
+            self.children.append(child_node)
+        elif child_node not in self.children:
+            self.children.append(child_node)
+        else: 
+            return
+        """
+    
+    def get_children(self): 
+        return self.children
+    
+    def clear_children(self): 
+        self.children = tuple()
+
+    def set_parent(self, parent): 
+        self.parent = parent
+
+    def get_parent(self): 
+        return self.parent
        
 def make_paper_from_query(query):
     message = query['message']
@@ -117,12 +164,23 @@ def query_from_DOI(doi):
 def surf(current_paper, starting_papers, seen_DOIs, seen_papers, cr, back_to_start_weight=0.15, 
          keyword_discard=0.8, keywords=[]):
     
+    class SurfWrapper(): 
+        def __init__(self, paper: PaperNode, is_back_to_start: bool): 
+            self._paper = paper
+            self._is_back_to_start = is_back_to_start
+        
+        def is_back_to_start(self): 
+            return self._is_back_to_start
+        
+        def get_paper_node(self): 
+            return self._paper
+        
     if not current_paper.get_references(): 
         print(f"Current paper does not have references on system: {current_paper.get_title()}")
-        return choice(list(starting_papers))
+        return SurfWrapper(choice(list(starting_papers)),True)
      
     if random() < back_to_start_weight: 
-        return choice(list(starting_papers))
+        return SurfWrapper(choice(list(starting_papers)),True)
     
     for _ in range(10): 
         random_reference = choice(current_paper.get_references())
@@ -140,7 +198,7 @@ def surf(current_paper, starting_papers, seen_DOIs, seen_papers, cr, back_to_sta
             try: 
                 query = query_from_DOI(doi)
             except: 
-                return choice(list(starting_papers))
+                return SurfWrapper(choice(list(starting_papers)),True)
             try:
                 random_paper = make_paper_from_query(query)
             except: 
@@ -151,7 +209,7 @@ def surf(current_paper, starting_papers, seen_DOIs, seen_papers, cr, back_to_sta
             print(f"Paper already seen: {random_reference.get_title()}")
             random_paper = next(x for x in seen_papers if x.get_DOI() == doi)
         
-        return random_paper
+        return SurfWrapper(random_paper,False)
         
         """
         title = random_reference.get_title()
@@ -162,7 +220,24 @@ def surf(current_paper, starting_papers, seen_DOIs, seen_papers, cr, back_to_sta
                     return random_paper
             
         """
-    return choice(list(starting_papers))
+    return SurfWrapper(choice(list(starting_papers)), False)
+
+def copy_node(node): 
+    """
+    Return deep copy of node
+    """
+    new_node = PaperNode(node.get_DOI(), node.get_title(), node._author, 
+                         node.get_year(), parent = node.get_parent())
+    return new_node
+
+def walk_tree(node): 
+    if not node.get_parent(): 
+        return node
+    else: 
+        new_node = copy_node(node.get_parent())
+        new_node.clear_children()
+        new_node.add_children(node.get_children())
+        return walk_tree(new_node)
 
 def main(): 
     cr = Crossref()
@@ -191,21 +266,24 @@ def main():
 
 
     paper_pointer = choice(list(starting_papers))
-    for _ in range(300): 
+    for _ in range(10): 
         print(f"iteration {_}")
-        new_paper = surf(paper_pointer, starting_papers, seen_DOIs, seen_papers, cr=cr,
+        new_wrapped_paper = surf(paper_pointer, starting_papers, seen_DOIs, seen_papers, cr=cr,
                          keywords=['pharmacokinetics', 'pharmacodynamics'])
+        new_paper = new_wrapped_paper.get_paper_node()
+        if not new_wrapped_paper.is_back_to_start(): 
+            paper_pointer.add_child_node(new_paper)
 
         if new_paper not in starting_papers: 
             if new_paper in tree:
                 # if we already have a node for this paper in tree, add another node only
                 # if parent different to previously recorded nodes
                 previous_nodes = [i for i in tree if i == new_paper]
-                if not any([True for i in previous_nodes if i.parent == paper_pointer]): 
-                    new_paper.parent = paper_pointer
+                if not any([True for i in previous_nodes if i.get_parent() == paper_pointer]): 
+                    new_paper.set_parent(paper_pointer)
                     tree.append(new_paper)
             else: 
-                new_paper.parent = paper_pointer
+                new_paper.set_parent(paper_pointer)
                 tree.append(new_paper)
             if new_paper not in seen_papers: 
                 paper_counter[new_paper] = 1
@@ -224,12 +302,24 @@ def main():
     sorted_paper_counter = sorted(paper_counter.items(), key=lambda item: item[1], reverse=True)
 
     for i,j in sorted_paper_counter: 
-        print(f"Paper {i.get_title()} DOI {i.get_DOI()} seen {j} times")
+        print(f"Paper {i.make_name()} {i.get_title()} DOI {i.get_DOI()} seen {j} times")
 
+    """
     for top_paper,_ in sorted_paper_counter[:3]: 
         for tree_node in tree:
             if top_paper == tree_node: 
                 for pre, fill, node in RenderTree(tree_node): 
+                    treestr = u"%s%s" % (pre, node.name)
+                    print(treestr.ljust(8), node.make_name())
+    for root in starting_papers: 
+        for pre, fill, node in RenderTree(root): 
+            treestr = u"%s%s" % (pre, node.name)
+            print(treestr.ljust(8), node.make_name())
+    """
+
+    for top_paper,_ in sorted_paper_counter: 
+        clean_root = walk_tree(top_paper)
+        for pre, fill, node in RenderTree(clean_root): 
                     treestr = u"%s%s" % (pre, node.name)
                     print(treestr.ljust(8), node.make_name())
 
