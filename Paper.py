5e6ee9ff655594b639c7f6601932449d0fa08418
Nada Reza <nada.reza@liverpool.ac.uk>
2023-04-27 17:19:45 +0100

    starting DAG

---
+++ Paper.py
@@ -0,0 +1,249 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Filename: 	Paper.py
+# Author: 	Alessandro Gerada, Nada Reza
+# Date: 	2023-04-25
+# Copyright: 	Alessandro Gerada 2023
+# Email: 	alessandro.gerada@liverpool.ac.uk
+
+"""Paper and PaperNode classes"""
+
+from anytree import NodeMixin
+import networkx as nx 
+from unidecode import unidecode
+
+class Paper:
+    def __init__(self, DOI, title, author, year, references = None):
+        self._DOI = DOI
+        self._title = title[0] if title else None
+        self._author = author
+        self._year = year
+        self._name = self.make_name()
+        self._references = []
+        if references:
+            self.add_references(references)
+    
+    def add_references(self, references):
+        for i in references:
+            doi = i['DOI'] if 'DOI' in i else None
+            title = i['article-title'] if 'article-title' in i else None
+            author = i['author'] if 'author' in i else None
+            year = i['year'] if 'year' in i else None
+            ref = Paper(doi, title, author, year)
+            self._references.append(ref)                     
+    
+    def __repr__(self) -> str:
+        return f"""
+        Paper {self._DOI}, author: {self.get_first_author()}, year: {self._year},
+        title: {self._title}
+        {len(self._references)} references
+        """
+    
+    def __hash__(self): 
+        return hash(self._DOI)
+    
+    def __eq__(self, other):
+        if isinstance(other, Paper): 
+            return self._DOI == other._DOI
+        return NotImplemented
+    
+    def get_first_author(self): 
+        try: 
+            author = self._author[0]['family']
+        except: 
+            author = None
+        return author
+    
+    def get_last_author(self):
+        try:
+            author = self._author[-1]['family']
+        except:
+            author = None
+        return author
+    
+    def get_all_authors(self):
+        try: 
+            authors_list = []
+            for author in self._author:
+                if author != self._author[0] & author != self._author[-1]:
+                    family_name = author['family']
+                    authors_list.append[family_name]
+        except:
+            author = None
+        return authors_list
+    
+    def get_references(self):
+        return self._references
+    
+    def get_title(self): 
+        return self._title
+    
+    def get_DOI(self):
+        return self._DOI
+    
+    def get_year(self):
+        return self._year   
+    
+    def make_name(self):
+        name = f"{self.get_first_author()} et al, {self.get_year()} ({self.get_DOI()})" 
+        return name
+    
+    def title_score(self, keywords):
+        title = self.get_title()
+        title_score = float(0)
+        if title:  
+            title = unidecode(title)
+            title = title.lower()
+            for keyword in keywords: 
+                if keyword[0] in title:
+                    value = float(keyword[1])
+                    title_score = title_score + value
+            return title_score
+        else:
+            title_score = float(0)
+    
+    def author_score(self, important_authors):
+        first_author = self.get_first_author()
+        last_author = self.get_last_author()
+        all_authors = self.get_all_authors()
+        author_score = float(0)
+
+        if first_author:
+            first_author = unidecode(first_author)
+            first_author = first_author.lower()
+            for author in important_authors:
+                if author in first_author:
+                    author_score = author_score + (25 * 0.375)
+        else:
+            author_score = float(0)
+        if last_author:
+            last_author = unidecode(last_author)
+            last_author = last_author.lower()
+            for author in important_authors:
+                if author in last_author:
+                    author_score = author_score + (25 * 0.375)
+        else:
+            author_score = float(0)
+        if all_authors:
+            for author in all_authors[:25]:
+                author = unidecode(author)
+                author = author.lower()
+            if author in all_authors:
+                author_score = author_score + (1 * 0.25)
+        else:
+            author_score = float(0)
+        if author_score > 25:
+            author_score = float(25)
+        return(author_score)
+    
+    def score_paper(self, keywords, important_authors):
+        wt_title_score = float(3 * self.title_score(keywords))
+        author_score = self.author_score(important_authors)
+        paper_score = (wt_title_score + author_score)
+        return(paper_score)
+        
+class PaperNode(Paper, NodeMixin):
+    def __init__(self, DOI, title, author, year, references = None, parent = None, children = None):
+        super().__init__(DOI, title, author, year, references)
+        self.name = self.make_name()
+        self.parent = parent
+        if children:
+            self.children = children
+    
+    def add_children(self, children):
+        if not children: 
+            return
+        self.children = children
+
+    def add_child_node(self, child_node): 
+        if not child_node: 
+            return
+        if type(child_node) != PaperNode: 
+            raise ValueError("Function .add_child_node() can only take PaperNode for parameter child_node") 
+        if child_node in self.get_children(): 
+            return
+        previous_children = self.get_children()
+        new_children = list(previous_children)
+        new_children.append(child_node)
+        self.children = new_children
+    
+    def get_children(self): 
+        return self.children
+    
+    def clear_children(self): 
+        self.children = tuple()
+    
+    def count_ancestors(self):
+        ancestors = list(self.ancestors)
+        num_ancestors = len(ancestors)
+        return num_ancestors
+    
+class DAGNode():
+    def __init__(self, name, parent : Paper = None, score = None):
+        self._name = name
+        if parent:
+            self._parent = parent.make_name()
+        if score:
+            self._score = score     
+    
+    def set_parent(self, parent_name): 
+        self._parent = parent_name
+    
+    def get_parent(self): 
+        return self._parent
+    
+    def get_name(self):
+        return self._name
+
+    def make_scoreless_edge(self):
+        parent = f"{self._parent}"
+        name = f"{self._name}"
+        dag_edge = (parent, name)
+        return(tuple(dag_edge))
+    
+    def make_scored_edge(self):
+        parent = f"{self._parent}"
+        name = f"{self._name}"
+        score = self._score
+        dag_edge = (parent, name, score)
+        return(tuple(dag_edge))
+    
+    def set_freq_score(self, freq_score):
+        self._freq_score = freq_score
+        return self._freq_score
+    
+    def set_depth_score(self, depth_score):
+        self._depth_score = depth_score
+        return self._depth_score
+    """""
+    def depth_score(self, starting_papers, node_list, paired_node_list):
+        root_nodes = []
+        for paper in starting_papers:
+            root_node = DAGNode(paper) 
+            root_nodes.append(root_node) 
+        self_node = DAGNode(self) 
+        DAG = nx.DiGraph
+        DAG.add_nodes_from(node_list)
+        DAG.add_edges_from(paired_node_list)
+        root_distance = [] 
+        for root in root_nodes:
+            shortest_root_node = nx.shortest_path(DAG, source=root, target=self_node)
+            root_distance.append[shortest_root_node]
+        max_depth = max(root_distance)
+        if max_depth > 25:
+            depth_score =  25 * 2
+        else:
+            depth_score = max_depth * 2
+        return(depth_score)
+    """ 
+    def set_score(self, score):
+        self._score = score
+        return(self._score)       
+
+
+
+
+
+        
+        
+    
--- main.py
+++ main.py
@@ -12,57 +12,15 @@ from habanero import Crossref
 import csv
 from datetime import datetime
 from random import random, choice
+from anytree import Node, RenderTree
+from unidecode import unidecode
+from Surf import SurfWrapper, BackToStart, InvalidReferences, NewPaper, PreviouslySeenPaper
+from Paper import Paper, PaperNode, DAGNode
+import networkx as nx 
+import matplotlib.pyplot as plt
+import networkx as nx 
 
-class Paper:
-    def __init__(self, DOI, title, author, year, references = None):
-        self._DOI = DOI
-        self._title = title[0] if title else None
-        self._author = author
-        self._year = year
-        self._references = []
-        if references:
-            self.add_references(references)
 
-    def add_references(self, references):
-        for i in references:
-            doi = i['DOI'] if 'DOI' in i else None
-            title = i['article-title'] if 'article-title' in i else None
-            author = i['author'] if 'author' in i else None
-            year = i['year'] if 'year' in i else None
-            ref = Paper(doi, title, author, year)
-            self._references.append(ref)
-    
-    def __repr__(self) -> str:
-        return f"""
-        Paper {self._DOI}, author: {self.get_first_author()}, year: {self._year},
-        title: {self._title}
-        {len(self._references)} references
-        """
-    
-    def __hash__(self): 
-        return hash(self._DOI)
-    
-    def __eq__(self, other):
-        if isinstance(other, Paper): 
-            return self._DOI == other._DOI
-        return NotImplemented
-    
-    def get_first_author(self): 
-        try: 
-            author = self._author[0]['family']
-        except: 
-            author = None
-        return author
-    
-    def get_references(self):
-        return self._references
-    
-    def get_title(self): 
-        return self._title
-    
-    def get_DOI(self):
-        return self._DOI
-    
 def make_paper_from_query(query):
     message = query['message']
     doi = message['DOI']
@@ -92,18 +50,29 @@ def query_from_DOI(doi):
     print(f"Unable to pull {doi}")
     return None
 
+def make_dagnode_from_paper(paper_name):
+    dagnode = DAGNode(paper_name)
+    return(dagnode)
+
+def get_dagnode(paper: Paper):
+    id = paper.make_name()
+    return(tuple(id, ))
+
 def surf(current_paper, starting_papers, seen_DOIs, seen_papers, cr, back_to_start_weight=0.15, 
          keyword_discard=0.8, keywords=[]):
-    
+        
     if not current_paper.get_references(): 
         print(f"Current paper does not have references on system: {current_paper.get_title()}")
-        return choice(list(starting_papers))
-     
+        return SurfWrapper(choice(list(starting_papers)), 
+                           action=InvalidReferences())
+    
     if random() < back_to_start_weight: 
-        return choice(list(starting_papers))
+        return SurfWrapper(choice(list(starting_papers)),
+                           action=BackToStart())
     
     for _ in range(10): 
         random_reference = choice(current_paper.get_references())
+
         # if we have already seen paper, don't download again
 
         doi = random_reference.get_DOI()
@@ -118,29 +87,43 @@ def surf(current_paper, starting_papers, seen_DOIs, seen_papers, cr, back_to_sta
             try: 
                 query = query_from_DOI(doi)
             except: 
-                return choice(list(starting_papers))
+                print(f"Unable to get query for: {random_reference.get_title()}")
+                continue
             try:
                 random_paper = make_paper_from_query(query)
+                return SurfWrapper(random_paper, 
+                                   action=NewPaper())
             except: 
-                print(f"Unable to get useful query for: {random_reference.get_title()}")
+                print(f"Unable to make paper from query for: {random_reference.get_title()}")
                 continue
 
         else: 
             print(f"Paper already seen: {random_reference.get_title()}")
             random_paper = next(x for x in seen_papers if x.get_DOI() == doi)
-        
-        return random_paper
-        
-        """
-        title = random_reference.get_title()
-        if title: 
-            title = title.lower()
-            for i in keywords: 
-                if i in title:
-                    return random_paper
-            
-        """
-    return choice(list(starting_papers))
+            return SurfWrapper(random_paper, 
+                               action=PreviouslySeenPaper())
+      
+    return SurfWrapper(choice(list(starting_papers)), 
+                       action=BackToStart())
+
+"""""
+def copy_node(node): 
+    
+    Return deep copy of node
+
+    new_node = PaperNode(node.get_DOI(), node.get_title(), node._author, 
+                         node.get_year(), parent = node.get_parent())
+    return new_node
+
+def walk_tree(node): 
+    if not node.get_parent(): 
+        return node
+    else: 
+        new_node = copy_node(node.get_parent())
+        new_node.clear_children()
+        new_node.add_children(node.get_children())
+        return walk_tree(new_node)
+"""
 
 def main(): 
     cr = Crossref()
@@ -159,34 +142,101 @@ def main():
     starting_papers = set()
     seen_papers = set()
     paper_counter = dict()
+    node_list = set()
+    paired_node_list = dict()
+    edge_list = dict()
+
+    KEYWORDS = 'keywords.csv'
+    IMPORTANT_AUTHORS = 'important_authors.csv'
+
+    keywords = [] 
+    important_authors = [] 
+
+    with open(KEYWORDS, 'r') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            keyterm = row['keyterms']
+            value = row['value']
+            keyword = [unidecode(keyterm).lower(), value]
+            keywords.append(keyword)
+    
+    with open(IMPORTANT_AUTHORS, 'r') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            author = row['Last']
+            author = unidecode(author)
+            author = author.lower()
+            important_authors.append(author)
 
     for i in starting_DOIs:
         result = query_from_DOI(i)
         paper = make_paper_from_query(result)
         starting_papers.add(paper)
-
-    """
-    for i in starting_papers: 
-        refs = i.get_references()
-        dois = [i.get_DOI() for i in refs]
-        for d in dois: 
-            if d: 
-                q = query_from_DOI(d)
-            else: 
-                q = None
-            if q: 
-                new_paper = make_paper_from_query(q)
-                print(new_paper)
-            else: 
-                print(q)
-    """
-
+        paper_name = paper.make_name()
+        dag_node = make_dagnode_from_paper(paper_name)
+        node_list.add(dag_node)
+        try:
+            first_author = paper.get_first_author()
+            first_author = unidecode(first_author)
+            first_author = first_author.lower()
+            if first_author not in important_authors:
+                important_authors.append(first_author)
+        except:
+            pass
+        try:
+            last_author = paper.get_last_author()
+            last_author = unidecode(last_author)
+            last_author = last_author.lower()
+            if last_author not in important_authors:
+                important_authors.append(last_author)
+        except:
+            pass 
 
     paper_pointer = choice(list(starting_papers))
-    for _ in range(10): 
+    for _ in range(50): 
         print(f"iteration {_}")
-        new_paper = surf(paper_pointer, starting_papers, seen_DOIs, seen_papers, cr=cr,
-                         keywords=['pharmacokinetics', 'pharmacodynamics'])
+        new_wrapped_paper = surf(paper_pointer, starting_papers, seen_DOIs, seen_papers, cr=cr,
+                                 back_to_start_weight=0.15)
+        new_paper = new_wrapped_paper.get_paper()
+        new_paper_score = new_paper.score_paper(keywords, important_authors)
+        new_paper_name = new_paper.make_name()
+        new_node = make_dagnode_from_paper(new_paper_name)
+        
+
+           #if the paper scores very low from title and authors, skip over it, likely irrelevant 
+        if new_paper_score < 10:
+             print(f"""
+             Low paper score: {new_paper.get_title} by {new_paper.get_first_author()}, 
+             Total ={new_paper_score}, 
+             Title = {new_paper.title_score(keywords)}, 
+             Author = {new_paper.author_score(important_authors)} 
+             - therefore likely irrelevant
+             """)
+             continue
+            # choice list starting_papers vs surf vs choice list seen_papers?? vs go back 'Dal segno al coda'
+        else:
+            print(f"""
+            Great paper score! {new_paper.get_title} by {new_paper.get_first_author()}, 
+            Total ={new_paper_score}, 
+            Title = {new_paper.title_score(keywords)}, 
+            Author = {new_paper.author_score(important_authors)} 
+            - paper included""")
+
+        if new_node not in node_list:
+            node_list.add(new_node)
+
+        if not new_wrapped_paper.is_back_to_start(): 
+            parent_name = paper_pointer.make_name()
+            new_node.set_parent(parent_name)
+            new_edge = new_node.make_scoreless_edge()
+            if new_paper_name not in paired_node_list:
+                paired_node_list[new_paper_name] = []
+                paired_node_list[new_paper_name].append(new_edge)
+            else:
+                if new_edge not in paired_node_list[new_paper_name]:
+                    paired_node_list[new_paper_name].append(new_edge)
+                else:
+                    pass
         
         if new_paper not in starting_papers: 
             if new_paper not in seen_papers: 
@@ -195,7 +245,7 @@ def main():
                 seen_papers.add(new_paper)
             else: 
                 paper_counter[new_paper] += 1
-        
+     
         if new_paper.get_references(): 
             paper_pointer = new_paper
         elif seen_papers: 
@@ -203,16 +253,74 @@ def main():
         else: 
             paper_pointer = choice(list(starting_papers))
         
-    for i,j in sorted(paper_counter.items(), key=lambda item: item[1]): 
-        print(f"Paper {i.get_title()} DOI {i.get_DOI()} seen {j} times")
+    sorted_paper_counter = sorted(paper_counter.items(), key=lambda item: item[1], reverse=True)
+
+    for i,j in sorted_paper_counter: 
+        print(f"Paper {i.make_name()} {i.get_title()} DOI {i.get_DOI()} seen {j} times")
+
+    concat_paired_nodes = []
+    root_nodes = []
+    for paper_name in paired_node_list:
+        for pair in paired_node_list[paper_name]:
+            concat_paired_nodes.append(pair)
+    for paper in starting_papers:
+        paper_name = paper.make_name()
+        if paper_name in node_list:
+            root_nodes.append(paper_name)
+    scoring_DAG = nx.DiGraph()
+    scoring_DAG.add_nodes_from(node_list)
+    scoring_DAG.add_edges_from(concat_paired_nodes)
+    nx.draw(scoring_DAG)
+    plt.show()
+    for paper_name in node_list:
+        root_distances = [] 
+        for root in root_nodes:
+            shortest_root_node = nx.shortest_path_length(scoring_DAG, source=root, target=paper_name)
+            print(f"shortest path:{shortest_root_node}")
+            root_distances.append[shortest_root_node]
+        print("hello!")
+        print(root_distances)
+        depth_score = max(root_distances)
+        freq_score = len(paired_node_list[paper_name])
+        weight_score = freq_score + depth_score
+        paper_name.set_score(weight_score)
+        scored_edge = paper_name.make_scored_edge()
+        edge_list[paper](scored_edge) 
+
+    DAG = nx.DiGraph()
+    DAG.add_nodes_from(edge_list)
+    DAG.add_edges_from(edge_list)
+    nx.draw_networkx(DAG, pos)
 
     with open('rs_output_10.csv', 'w', newline='') as csvfile:
 
         writer = csv.writer(csvfile, delimiter=",")
-        writer.writerow(['DOI', 'title', 'times_seen'])
+        writer.writerow(['DOI', 'author', 'title', 'times_seen'])
         for paper,times_seen in paper_counter.items(): 
             writer.writerow([paper.get_DOI(), 
                              paper.get_title(), 
+                             paper.get_first_author(),
                              times_seen])
 
 main()
+
+"""""
+    for leaf in tree:
+     try: 
+         print(f" ID:{leaf.name}, parent:{leaf.parent.name}, children{leaf.children.name}")
+     except: 
+         continue
+
+    tree_roots = set()
+    for leaf in tree:
+        if leaf.parent == None:
+            tree_roots.add(leaf)
+        else: 
+            continue
+    
+    for root in tree_roots:
+        print(f"Root: {root}")
+        for pre, fill, node in RenderTree(root): 
+                    treestr = u"%s%s" % (pre, node.name)
+                    print(treestr.ljust(8))
+"""
